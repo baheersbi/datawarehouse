d150f5be8be3bc04e51ad56958cedcae1e374519
baheersbi <161305857+baheersbi@users.noreply.github.com>
2024-04-16 15:28:46 +0200

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,10 @@
+create database bugbond;
+
+use bugbond;
+
+CREATE TABLE Stores (
+    StoreID INT AUTO_INCREMENT PRIMARY KEY,
+    StoreName VARCHAR(255) NOT NULL,
+    Location VARCHAR(255) NOT NULL,
+    Contact VARCHAR(255)
+);
