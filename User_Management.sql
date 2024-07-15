8c4ebba9be3d20dd5eec4626fe91343f0b19379e
baheersbi <baseer.baheer@berlinsbi.com>
2024-07-15 20:50:05 +0200

    Create User_Management.sql

---
+++ User_Management.sql
@@ -0,0 +1,8 @@
+1. Create a user
+```bash
+CREATE USER 'akash'@'localhost' IDENTIFIED BY 'password';
+```
+> **Note:** 
+> 1. ```'akash'@'localhost'```: The user (```akash```) can access MySQL only from the ```localhost```
+> 2. 
+  4. ```localhost``` is not included in the ```%``` wildcard.
